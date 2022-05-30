020d180705d7d5d47cd56e2a9dcf78313d5a3383
pkrfc <Happyz1804>
2022-05-30 18:31:08 +0300

    1t

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,6 +8,7 @@ from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
+
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Categories, Comment, Genres, Review, Title
@@ -166,8 +167,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        queryset = title.reviews.all()
-        return queryset
+        return title.reviews.all()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -183,5 +183,4 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'),
                                    title=self.kwargs.get('title_id'))
-        queryset = review.comments.all()
-        return queryset
+        return review.comments.all()
