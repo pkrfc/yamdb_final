f56a364c7571a541c611ad79ee570da5b0dd15c6
pkrfc <Happyz1804>
2022-06-01 17:13:44 +0300

    redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -127,8 +127,8 @@ USE_L10N = True
 USE_TZ = False
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
