779b19b7d260bad95fde3c68738baf776bb9718c
pkrfc <Happyz1804>
2022-06-01 18:08:46 +0300

    environ

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 import environ
 from django.core.management.utils import get_random_secret_key
 
-
 env = environ.Env(
     DEBUG=(bool, True),
     SECRET_KEY=(str, '*'),
