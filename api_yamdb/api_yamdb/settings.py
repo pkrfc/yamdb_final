fc45265b201f50566f04ea66bb0008aaf2f49aa5
pkrfc <Happyz1804>
2022-06-01 17:27:52 +0300

    redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ',').split()
 
 AUTH_USER_MODEL = 'users.User'
 
